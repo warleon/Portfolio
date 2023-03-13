d7c5ebf1a75398a5ed270eb9edcb92b28b884014
warleon <51892390+warleon@users.noreply.github.com>
2023-03-13 01:25:04 -0500

    looping

--- src/Components/roulette.js
+++ src/Components/roulette.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 
 
@@ -9,20 +9,53 @@ const Wrapper = styled.div`
 	overflow-x: auto;
 	scroll-snap-type: inline mandatory;
 `
-const Item = styled.div`
+const ItemStart = styled.div`
 	scroll-snap-align:start;
+`
+const ItemEnd = styled.div`
+	scroll-snap-align:end;
 `
 
 function Roulette(props) {
-
+	const wrapper = useRef(null)
+	let scroll = 0;
+	let changed = false
+	const handleScroll = (e) => {
+		const dist = 5
+		var width = wrapper.current.scrollWidth - wrapper.current.clientWidth;
+		let currScroll = wrapper.current.scrollLeft
+		let diff = currScroll - scroll
+		if (diff > 0) {
+			console.log([scroll, currScroll, diff, width, "scrolling right"])
+			if (currScroll >= width - dist && !changed) {
+				wrapper.current.scrollLeft = 0
+				changed = true;
+			} else
+				changed = false;
+		}
+		if (diff < 0) {
+			console.log([scroll, currScroll, diff, width, "scrolling left"])
+			if (currScroll <= dist && !changed) {
+				wrapper.current.scrollLeft = width
+				changed = true;
+			} else
+				changed = false;
+		}
+		scroll = currScroll
+	}
 	return (
-		<Wrapper {...props}>
-			{props.children.map((child, i) => (
-				<Item key={i}>
+		<Wrapper {...props} ref={wrapper} onScroll={handleScroll}>
+			{props.children.map((child, i) => {
+				if (i == props.children.length - 1)
+					return <ItemEnd key={i} >
+						{child}
+					</ItemEnd>
+				return <ItemStart key={i}>
 					{child}
-				</Item>
-			))}
-		</Wrapper>
+				</ItemStart>
+			}
+			)}
+		</Wrapper >
 	)
 
 }
