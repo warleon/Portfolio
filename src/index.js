0cf68096b157e248d56be0083a059afdcc775864
warleon <51892390+warleon@users.noreply.github.com>
2023-04-22 00:17:20 -0500

    port to normal react

--- src/index.js
+++ src/index.js
@@ -1,41 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
 
-// Your top level component
-import App from './App'
-
-// Export your top level component as JSX (for static rendering)
-export default App
-
-// Render your app
-if (typeof document !== 'undefined') {
-  const target = document.getElementById('root')
-
-  const renderMethod = target.hasChildNodes()
-    ? ReactDOM.hydrate
-    : ReactDOM.render
-
-  const render = Comp => {
-    renderMethod(
-      <AppContainer>
-        <HashRouter>
-          <Comp />
-        </HashRouter>
-      </AppContainer>,
-      target
-    )
-  }
-
-  // Render!
-  render(App)
-
-  // Hot Module Replacement
-  if (module && module.hot) {
-    module.hot.accept('./App', () => {
-      render(App)
-    })
-  }
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
