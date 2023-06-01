31bcf20b6856ddf7b1bc0af541864a23b9393044
warleon <51892390+warleon@users.noreply.github.com>
2023-06-01 12:54:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const fs = require('fs');
+const https = require('https');
 const path = require('path');
 
+const certificate = '/etc/letsencrypt/live/tan.gozu.lat/fullchain.pem'
+const certificate_key = '/etc/letsencrypt/live/tan.gozu.lat/privkey.pem'
 const app = express()
 const port = 3000
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -10,6 +15,12 @@ app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
+
+const server = https.createServer({
+	key: fs.readFileSync(certificate_key, 'utf8'),
+	cert: fs.readFileSync(certificate, 'utf8')
+}, app);
+
+await server.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
-})
+});
